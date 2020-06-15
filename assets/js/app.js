57b4aaea815a1c1b64882f68f336ef6f2690c9af
eradicador <janisepinto@yahoo.com>
2020-06-15 16:54:08 -0400

    added index file, assets, css, js and sounds folders

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+var questions = [
+    {
+      title: "Example Question 1:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    },
+    {
+      title: "Example Question 2:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    }
+  ];
+  var containerEl = document.querySelector(".container")
+
+  var timer = document.querySelector(".timer")
+  
+  var startBtn = document.createElement("button");
+
+  var questionText = document.createElement("p");
+
+  var timer = 80;
+
+  var Index = 0;
+
