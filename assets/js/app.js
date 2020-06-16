9bcc0d55a95423e69cc86390cb7b6fb021f4c640
eradicador <janisepinto@yahoo.com>
2020-06-15 22:40:58 -0400

    fixed js file

---
+++ assets/js/app.js
@@ -0,0 +1,71 @@
+var containerEl = document.querySelector(".container");
+var timer = document.querySelector(".timer");
+var startText = document.createElement("h1");
+var startBtn = document.createElement("button");
+var questionText = document.createElement("p");
+var startBtn = document.createElement("button");
+var questionText = document.createElement("p");
+var timer = 80;
+var Index = 0;
+
+var questions = [
+    {
+        title: "Example Question 1:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    },
+    {
+        title: "Example Question 2:",
+        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+        answer: "answer from choices"
+    }
+];
+
+openingPage()
+
+function openingPage() {
+    startText.textContent = "Welcome to my Quiz";
+    startBtn.textContent = "Start Quiz";
+    containerEl.appendChild(startText);
+    containerEl.appendChild(startBtn);
+}
+
+function startQuiz() {
+    showTimer();
+    nextQuestion();
+}
+
+function showTimer() {
+    timerDisplay.textContent = timer;
+    var timeInterval = setInterval(function () {
+        timer--;
+        timerDisplay.textContent = timer;
+        if (timer === 0) {
+            clearInterval(timeInterval)
+        }
+    }, 1000)
+
+    function nextQuestion() {
+        var currentQuestion = questions[index];
+        containerE1.textContent = "";
+        questionText.textContent = currentQuestion.title;
+        containerEl.appendChild(questionText);
+        var answersDiv = document.createElement("div");
+
+        for (let i = 0; 1 < currentQuestion.choices.length; i++) {
+            var answerBtn = document.createElement("button");
+            answerBtn.classList.add("choiceBtn");
+            answerBtn.textContent = currentQuestion.choices[i];
+            answerDiv.appendChild(answerBtn);
+        }
+        containerEl.appendChild(answersDiv);
+    }
+};
+
+function checkAnswer(event) {
+    if (event.target.matches(".choiceBtn")) {
+    }
+
+    startBtn.addEventListener("click", startQuiz())
+}
+openingPage();
